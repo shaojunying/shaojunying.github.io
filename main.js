e2a0393c2be89bbe26d6dcd865ee871bb5db24d4
邵俊颖 <shaojunying@bupt.edu.cn>
2018-09-18 10:31:55 +0800

    github的简单搜索

--- main.js
+++ main.js
@@ -15,7 +15,21 @@ function() {
         card.slideUp(100);
 })
 
-$.ajax('https://api.github.com/users/shaojunying')
-    .done(function(data) {
-        console.log("data:",data);
-    })
+var form = $('search');
+var input = $('input#username');
+var result = $('#result');
+var username;
+form.on('submit',function(e){
+    e.preventDefault();
+    username= input.val();
+    $.ajax('https://api.github.com/users/'+username)
+    .done(function(data){
+        var html =
+        '<div>用户名：'+data.login +'</div>'+
+        '<div>name' + data.name+'</div>'+
+        '<div>介绍' + data.bio+'</div>'+
+        '<div>地点' + data.location+'</div>'+
+        '<div>博客' + data.blog+'</div>'
+        result.html(html);
+    });
+})
